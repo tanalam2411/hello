4118facbae265ec343a4c22dee8a71094ab6af7e
tanalam2411 <tan24alam11@gmail.com>
2022-10-30 17:31:33 +0530

    Oct 30

--- hello.go
+++ hello.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/go-cmp/cmp"
 	"github.com/tanalam2411/hello/morestrings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
